30f826d9857e94870f6a993dac4607ab4de8911c
wildcard329 <enkiduwildman@gmail.com>
2020-04-11 08:48:35 -0700

    styling

--- src/components/pokedex/pokemonDisplay.jsx
+++ src/components/pokedex/pokemonDisplay.jsx
@@ -9,20 +9,9 @@ const useStyles = makeStyles(() => {
     return {
         "image-display": {
             background:  "gray",
-            width: "250%",
+            width: "450%",
             marginTop: "20%",
             marginLeft: "10%"
-        },
-        "text-display": {
-            background: "lightgray",
-            padding: "5%",
-            marginLeft: "80%",
-            marginBottom: "7%",
-            width: "100%"
-        },
-        "flex-group": {
-            display: "flex",
-            flexDirection: "column"
         }
     }
 })
@@ -33,12 +22,8 @@ const PokemonDisplay = props => {
     return (
         <div>
             {props.pokemonStatus && 
-                                <div className={classes["flex-group"]}>
                                     <img  className={classes["image-display"]} 
-                                    src = {props.pokemonStatus.sprites.front_default} />
-                                    <h1 className={classes["text-display"]}>{props.pokemonStatus.name.toUpperCase()}</h1>
-                                </div>
-                                }
+                                    src = {props.pokemonStatus.sprites.front_default} />}
         </div>
     )
 }
